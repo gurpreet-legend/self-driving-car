e99ff51723f57b28551f1665419e30d013a9bb57
Gurpreet Singh <lostlegend2701@gmail.com>
2024-02-01 18:33:56 +0530

    traffic added

--- main.js
+++ main.js
@@ -3,13 +3,20 @@ canvas.width = 200
 
 const ctx = canvas.getContext("2d")
 const road = new Road(canvas.width/2, canvas.width*0.9)
-const car = new Car(road.getLaneCenter(1), 100, 30, 50)
-car.draw(ctx)
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS")
+const traffic = [
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)
+]
 
 animate();
 
 function animate(){
-    car.update(road.borders);
+    // animating traffic
+    for(let i=0; i<traffic.length; i++) {
+        traffic[i].update(road.borders, []);
+    }
+    // animating our car
+    car.update(road.borders, traffic);
 
     canvas.height = window.innerHeight // This can resize the canvas as well as resets it
 
@@ -17,7 +24,10 @@ function animate(){
     ctx.translate(0, -car.y + canvas.height*0.7)
 
     road.draw(ctx);
-    car.draw(ctx);
+    for(let i=0; i<traffic.length; i++) { //traffic
+        traffic[i].draw(ctx, "red");
+    }
+    car.draw(ctx, "blue"); //car
 
     ctx.restore()
     requestAnimationFrame(animate);
--- sensor.js
+++ sensor.js
@@ -9,17 +9,17 @@ class Sensor{
         this.readings=[]
     }
 
-    update(roadBorders){
+    update(roadBorders, traffic){
         this.#castRays()
         this.readings = []
         for(let i=0; i<this.rays.length; i++){
             this.readings.push(
-                this.#getReadings(this.rays[i], roadBorders)
+                this.#getReadings(this.rays[i], roadBorders, traffic)
             )
         }
     }
 
-    #getReadings(ray, roadBorders) {
+    #getReadings(ray, roadBorders, traffic) {
         let touches = []
 
         for(let i=0; i<roadBorders.length; i++) {
@@ -31,6 +31,21 @@ class Sensor{
             )
             if(touch) {
                 touches.push(touch)
+            }
+        }
+
+        for(let i=0; i<traffic.length; i++){
+            const poly = traffic[i].polygon
+            for(let j=0; j<poly.length;j++){
+                const value = getIntersection(
+                    ray[0],
+                    ray[1],
+                    poly[j],
+                    poly[(j+1)%poly.length]
+                )
+                if(value) {
+                    touches.push(value)
+                }
             }
         }
 
@@ -63,7 +78,7 @@ class Sensor{
     }
 
     draw(ctx) {
-        for(let i=0; i<this.rays.length; i++) {
+        for(let i=0; i<this.rayCount; i++) {
             let end = this.rays[i][1]
             if(this.readings[i]) {
                 end = this.readings[i]
